632a28801c46db59e72dbd6917faa21b52b0472a
Michael Stokley <michael@michaelstokley.com>
2016-05-17 22:29:04 -0700

    fixing import order

--- src/scrape.py
+++ src/scrape.py
@@ -1,9 +1,9 @@
+from __future__ import print_function
 import io
 import os
 import re
 import requests
 import sys
-from __future__ import print_function
 from bs4 import BeautifulSoup
 from collections import deque
 
